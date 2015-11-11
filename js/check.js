f50fc178f572b3def4f3816a3968de1995a18b94
zakrevich <zakrevich@yahoo.com>
2015-11-11 22:40:37 +0300

    Update check.js

--- js/check.js
+++ js/check.js
@@ -1,1 +1,20 @@
+getMessage(a:*, b:*=):string
 
+if a=boolean 
+ {
+  return "Я попал в [b]"
+}
+
+else if a=number
+{
+  return "Я прыгнул на [a] * 100 сантиметров"
+}
+else if a=array {
+ return "Я прошел [sum] шагов"
+}
+else if a=array || b=array {
+  return "Я прошел [length] метров"
+}
+else {
+  return "Я никуда не попал"
+}
