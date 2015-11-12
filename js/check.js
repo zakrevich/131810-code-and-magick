81b6015155112addb3b7b4d0a5771189afada571
zakrevich <zakrevich@Daryas-MacBook-Air.local>
2015-11-12 21:20:52 +0300

    working code

--- js/check.js
+++ js/check.js
@@ -1,25 +1,31 @@
 function getMessage(a,b) {
 
-}
-var a
-var b
+  if (typeof a=== "boolean") {
+    if (a===true) {
+      return "Я попал в " + b
+    }
+    else {
+       return "Я никуда не попал"
+    }
+  }
 
+  else if (typeof a=== "number") {
+    return "Я прыгнул на " + a * 100 + " сантиметров"
+  }
 
-if (a)
-{
-  return "Я попал в [b]"
-}
+  else if (typeof a==="object" && typeof b==="object") {
+    var length = 0
+    for (var i = 0; i < a.length && i < b.length; i++) {
+    length = length + a[i] * b[i];
+    }
+    return "Я прошёл " + length + " метров"
+  }
 
-else if typeof a=== "number"
-{
-  return "Я прыгнул на [a] * 100 сантиметров"
-}
-else if a=array {
-return "Я прошел [sum] шагов"
-}
-else if a=array || b=array {
-  return "Я прошел [length] метров"
-}
-else {
-  return "Я никуда не попал"
+  else if (typeof a==="object") {
+    var sum = 0
+    for (var i = 0; i < a.length; i++) {
+    sum = sum + a[i];
+    }
+    return "Я прошёл " + sum + " шагов"
+  }
 }
