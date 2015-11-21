5ad61efd616e817817aeb6adb2895ee93ca0d4f7
zakrevich <zakrevich@Daryas-MacBook-Air.local>
2015-11-21 19:58:13 +0300

    cookies

--- js/form.js
+++ js/form.js
@@ -15,6 +15,17 @@
   var reviewText = formElement['review-text'];
 
   name.required = true;
+  name.value = docCookies.getItem('name');
+
+  function getRadioValue() {
+    for (var i = 0; i < stars.length; i++) {
+      if (stars[i].checked === true) {
+        return stars[i].value;
+      }
+    }
+
+    return null;
+  }
 
   function checkName() {
     var nameIsNotEmpty = (name.value !== '');
@@ -23,7 +34,7 @@
   }
 
   function checkStarsText() {
-    if (stars.value < 3) {
+    if (getRadioValue() < 3) {
       if (reviewText.value === '') {
         reviewText.required = true;
         fieldText.classList.remove('invisible');
@@ -61,13 +72,33 @@
   };
 
   for (var i = 0; i < stars.length; i++) {
+    if (stars[i].value === docCookies.getItem('stars')) {
+      stars[i].checked = true;
+    }
     stars[i].onchange = function() {
       removeAll();
     };
   }
 
+  formElement.onsubmit = function(evt) {
+    evt.preventDefault();
+    var now = new Date();
+    var lastBirth = new Date(now.getFullYear() - 1 + '-12-09');
+    var dateExpired = new Date(+now + +now - lastBirth);
+
+    document.cookie = 'name=' + name.value + ';expires=' + dateExpired;
+
+    var starsValue = getRadioValue();
+    if (starsValue === true) {
+      document.cookie = 'stars=' + starsValue + ';expires=' + dateExpired.toUTCString();
+    }
+
+    formElement.submit();
+  };
+
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
+    removeAll();
     formContainer.classList.remove('invisible');
     fieldText.classList.add('invisible');
   };
